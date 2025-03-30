1486e14d84a878a622c958bf573afd7a6ef199a1
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-31 00:29:10 +0530

    update vercel

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,12 @@ import connectDB from "./config/db.config.js";
 
 dotenv.config({ path: "./.env" });
 
+const PORT = process.env.PORT || 3000;
+
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
 })
 .catch((err) => {
