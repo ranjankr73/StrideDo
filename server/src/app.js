d0043a760c3c6b6d57b459db6de2570a4c0393b4
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-30 22:10:22 +0530

    update cors in app

--- server/src/app.js
+++ server/src/app.js
@@ -6,9 +6,15 @@ const app = express();
 
 //common middlewares
 app.use(cors({
-    origin: 'https://stridedo.vercel.app',
+    origin: ['https://stridedo.vercel.app'],
     credentials: true
 }));
+
+app.options('*', cors({
+    origin: ['https://stridedo.vercel.app'],
+    credentials: true
+}));
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb"}));
 app.use(express.static("public"));
