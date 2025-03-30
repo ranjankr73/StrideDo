3fca30248bd35524f1244ce121bba925340bae55
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-30 23:38:33 +0530

    update the vercel.json of server

--- server/src/app.js
+++ server/src/app.js
@@ -10,11 +10,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.options('*', cors({
-    origin: ['https://stridedo.vercel.app'],
-    credentials: true
-}));
-
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb"}));
 app.use(express.static("public"));
